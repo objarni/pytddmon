0e1f84429b084261e1831221245caaa008123fc5
Olof Bjarnason <olof.bjarnason@gmail.com>
2011-11-09 21:38:12 +0100

    Re-added missing systest files. Where did they go??

---
+++ systest/one_green_of_two_tests/test_code.py
@@ -0,0 +1,8 @@
+﻿import unittest
+
+class TestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertTrue(True)
+
+    def test_something_else(self):
+        self.assertTrue(False)
