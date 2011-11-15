8d8fbd913495edadfe3c46062e7842d5100e510a
Olof Bjarnason <olof.bjarnason@gmail.com>
2011-11-15 21:47:19 +0100

    Moved Fake classes into its own module

--- tests/file_strategies/test_recursive_glob_file_strategy.py
+++ tests/file_strategies/test_recursive_glob_file_strategy.py
@@ -1,21 +1,7 @@
 import unittest
 import os
 from pytddmon import RecursiveGlobFileStartegy
-
-class FakeHasher(object):
-    def __init__(self):
-        self.file_paths = {}
-    def __call__(self, file_path):
-        return self.file_paths.get(file_path, 0)
-    def change_file(self, file_path):
-        self.file_paths[file_path] = self.file_paths.get(file_path, 0) + 1
-
-class FakeErrorHasher(object):
-    def __call__(self, path):
-        raise IOError(
-            "OSError: [Errno 2] No such file or directory: '%s'" %
-            path
-        )
+from tests.fakes import FakeHasher
 
 class RecursiveGlobFileStartegyTest(unittest.TestCase):
 
--- tests/file_strategies/test_static_file_strategy.py
+++ tests/file_strategies/test_static_file_strategy.py
@@ -1,21 +1,8 @@
 import unittest
 import os
 from pytddmon import StaticFileStartegy
-
-class FakeHasher(object):
-    def __init__(self):
-        self.file_paths = {}
-    def __call__(self, file_path):
-        return self.file_paths.get(file_path, 0)
-    def change_file(self, file_path):
-        self.file_paths[file_path] = self.file_paths.get(file_path, 0) + 1
-class FakeErrorHasher(object):
-    def __call__(self, path):
-        raise IOError(
-            "OSError: [Errno 2] No such file or directory: '%s'" %
-            path
-        )
-        
+from tests.fakes import FakeHasher
+from tests.fakes import FakeErrorHasher
 
 class StaticFileStartegyTest(unittest.TestCase):
 
