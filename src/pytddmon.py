afebcdd21d8a5a77df05400447f743a791688f12
Olof Bjarnason <olof.bjarnason@gmail.com>
2013-11-19 11:40:35 +0000

    Moved out the contributors list to separate file

--- src/pytddmon.py
+++ src/pytddmon.py
@@ -2,29 +2,7 @@
 #coding: utf-8
 
 """
-COPYRIGHT (c) 2009, 2010, 2011, 2012
-.. in order of first contribution
-Olof Bjarnason
-    Initial proof-of-concept pygame implementation.
-Fredrik Wendt
-    Help with Tkinter implementation (replacing the pygame dependency)
-Krunoslav Saho
-    Added always-on-top to the pytddmon window
-Samuel Ytterbrink
-    Print(".") will not screw up test-counting (it did before)
-    Docstring support
-    Recursive discovery of tests
-    Refactoring to increase Pylint score from 6 to 9.5 out of 10 (!)
-    Numerous refactorings & other improvements
-Rafael Capucho
-    Python shebang at start of script, enabling "./pytddmon.py" on unix systems
-Ilian Iliev
-    Use integers instead of floats in file modified time (checksum calc)
-    Auto-update of text in Details window when the log changes
-Henrik Bohre
-    Status bar in pytddmon window, showing either last time tests were
-    run, or "Testing..." during a test run
-
+COPYRIGHT (c) 2009-2013
 
 LICENSE
 Permission is hereby granted, free of charge, to any person obtaining a copy
