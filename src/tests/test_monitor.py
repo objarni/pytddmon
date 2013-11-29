384068a7cda7f3d9e69c7fd2aee4bbbefe1042ef
Wari Wahab <wahabmw@i2r.a-star.edu.sg>
2013-11-29 19:14:35 +0000

    Removed a lot of duplicated code
    After simplification of functions to lambdas, we can remove a lot of
    code that is simpler to generalize.

--- src/tests/test_monitor.py
+++ src/tests/test_monitor.py
@@ -5,6 +5,14 @@ from pytddmon import Monitor
 
 class TestChangeDetection(unittest.TestCase):
 
+    def _set_up_monitor(self):
+        files = ['file']
+        file_finder = lambda: files
+        get_file_size = lambda x: 1
+        get_file_modification_time = lambda x: 1
+        monitor = Monitor(file_finder, get_file_size, get_file_modification_time)
+        return files, monitor
+
     def test_modification_time_changed(self):
         files = ['file']
         file_finder = lambda: files
@@ -19,44 +27,24 @@ class TestChangeDetection(unittest.TestCase):
         assert change_detected
 
     def test_nothing_changed(self):
-        files = ['file']
-        file_finder = lambda: files
-        get_file_size = lambda x: 1
-        get_file_modification_time = lambda x: 1
-
-        monitor = Monitor(file_finder, get_file_size, get_file_modification_time)
+        files, monitor = self._set_up_monitor()
         change_detected = monitor.look_for_changes()
         assert not change_detected
 
     def test_adding_file(self):
-        files = ['file']
-        file_finder = lambda: files
-        get_file_size = lambda x: 1
-        get_file_modification_time = lambda x: 1
-
-        monitor = Monitor(file_finder, get_file_size, get_file_modification_time)
+        files, monitor = self._set_up_monitor()
         files.append('file2')
         change_detected = monitor.look_for_changes()
         assert change_detected
 
     def test_renaming_file(self):
-        files = ['file']
-        file_finder = lambda: files
-        get_file_size = lambda x: 1
-        get_file_modification_time = lambda x: 1
-
-        monitor = Monitor(file_finder, get_file_size, get_file_modification_time)
+        files, monitor = self._set_up_monitor()
         files[0] = 'renamed'
         change_detected = monitor.look_for_changes()
         assert change_detected
 
     def test_change_is_only_detected_once(self):
-        files = ['file']
-        file_finder = lambda: files
-        get_file_size = lambda x: 1
-        get_file_modification_time = lambda x: 1
-
-        monitor = Monitor(file_finder, get_file_size, get_file_modification_time)
+        files, monitor = self._set_up_monitor()
         files[0] = 'changed'
         change_detected = monitor.look_for_changes()
         change_detected = monitor.look_for_changes()
