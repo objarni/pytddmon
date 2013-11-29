106bee4f27419b448a7b6bcbb63d0db52c907a2a
Wari Wahab <wahabmw@i2r.a-star.edu.sg>
2013-11-29 18:42:55 +0000

    Making tests clean
    * Tests are a first class citizen :)
    * Added double lines, and single lines where PEP8 requires it
    * Added a FIXME comment to code that does not work
    * Ensure classes are CamelCase
    * Removed ugly tabs and extra whitespaces
    
    PS: Now tests should look great :)
    
    Pulse
    - changing color resets pulse
    - dark after pulse
    - default color is green
    - no failing test picks green
    - no tests means green
    - one failing test picks red
    - pulse is not reset if colors stays same
    - starts with light color
    - two failing tests picks gray
    
    Change detection
    - adding file
    - change is only detected once
    - file order does not matter
    - file size changed
    - modification time changed
    - nothing changed
    - renaming file
    
    Pytddmon monitor communication
    - doesn't run tests when no change
    - runs each time a change is detected
    - runs tests at boot
    - runs tests when change detected
    - total tests is zero if no tests are run
    
    Status bar feature
    - pytddmon should have get status message function

--- src/tests/test_monitor.py
+++ src/tests/test_monitor.py
@@ -2,16 +2,21 @@
 import unittest
 from pytddmon import Monitor
 
-class test_change_detection(unittest.TestCase):
+
+class TestChangeDetection(unittest.TestCase):
 
     def test_modification_time_changed(self):
         def file_finder():
             return ['file']
+
         def get_file_size(file):
             return 1
+
         modtime = [1]
+
         def get_file_modification_time(file):
             return modtime[0]
+
         monitor = Monitor(file_finder, get_file_size, get_file_modification_time)
         modtime[0] = 2
         change_detected = monitor.look_for_changes()
@@ -20,8 +25,10 @@ class test_change_detection(unittest.TestCase):
     def test_nothing_changed(self):
         def file_finder():
             return ['file']
+
         def get_file_size(file):
             return 1
+
         def get_file_modification_time(file):
             return 1
         monitor = Monitor(file_finder, get_file_size, get_file_modification_time)
@@ -30,12 +37,16 @@ class test_change_detection(unittest.TestCase):
 
     def test_adding_file(self):
         files = ['file']
+
         def file_finder():
             return files
+
         def get_file_size(file):
             return 1
+
         def get_file_modification_time(file):
             return 1
+
         monitor = Monitor(file_finder, get_file_size, get_file_modification_time)
         files.append('file2')
         change_detected = monitor.look_for_changes()
@@ -43,10 +54,13 @@ class test_change_detection(unittest.TestCase):
 
     def test_renaming_file(self):
         files = ['file']
+
         def file_finder():
             return files
+
         def get_file_size(file):
             return 1
+
         def get_file_modification_time(file):
             return 1
         monitor = Monitor(file_finder, get_file_size, get_file_modification_time)
@@ -56,12 +70,16 @@ class test_change_detection(unittest.TestCase):
 
     def test_change_is_only_detected_once(self):
         files = ['file']
+
         def file_finder():
             return files
+
         def get_file_size(file):
             return 1
+
         def get_file_modification_time(file):
             return 1
+
         monitor = Monitor(file_finder, get_file_size, get_file_modification_time)
         files[0] = 'changed'
         change_detected = monitor.look_for_changes()
@@ -71,12 +89,16 @@ class test_change_detection(unittest.TestCase):
     def test_file_size_changed(self):
         files = ['file']
         filesize = [1]
+
         def file_finder():
             return files
+
         def get_file_size(file):
             return filesize[0]
+
         def get_file_modification_time(file):
             return 1
+
         monitor = Monitor(file_finder, get_file_size, get_file_modification_time)
         filesize[0] = 5
         change_detected = monitor.look_for_changes()
@@ -84,10 +106,13 @@ class test_change_detection(unittest.TestCase):
 
     def test_file_order_does_not_matter(self):
         files = ['file', 'file2']
+
         def file_finder():
             return files
+
         def get_file_size(file):
             return 1
+
         def get_file_modification_time(file):
             return 1
         monitor = Monitor(file_finder, get_file_size, get_file_modification_time)
