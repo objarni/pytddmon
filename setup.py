143d0a977b6fd3381167f606267fe82baf73997f
mriehl <maximilien.riehl@gmail.com>
2013-12-06 13:55:30 +0100

    adding setup.py with nosetest integration

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+if __name__ == '__main__':
+    setup(
+        name='pytddmon',
+        version='1.0.0',
+        description='continuous unit testing in Python',
+        long_description='Read the pytddmon blog and more documentation at http://pytddmon.org',
+        author="Olof Bjarnason",
+        author_email="olof.bjarnason@gmail.com",
+        license='MIT',
+        url='https://github.com/objarni/pytddmon',
+        scripts=['src/pytddmon.py'],
+        test_suite='nose.collector',
+        zip_safe=True
+    )
